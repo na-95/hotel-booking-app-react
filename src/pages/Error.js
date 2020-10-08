bc7ed06a15be4761dbc867e81333765decaa5f3d
nouraizasad <nouraizasad95@gmail.com>
2020-10-08 21:31:33 +0500

    Added React Routing

---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Error = () => {
+    return (
+        <div>
+            Hello from error pages
+        </div>
+    )
+}
+
+export default Error;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            Hello from home page
+        </div>
+    )
+}
+
+export default Home;
